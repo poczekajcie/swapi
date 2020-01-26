92796a7d679ac2447f9a08d1025a4761c5134d01
Jacek Krakowski <jacek.krakowski@apilia.pl>
2020-01-26 01:20:54 +0100

    cleanup

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,18 +1,5 @@
 import axios from 'axios';
 
-export const fetchPeople = () =>
-    async dispatch => {
-        const proxyurl = "https://cors-anywhere.herokuapp.com/";
-        const url = "https://swapi.co/api/people";
-        const response = await axios.get(proxyurl + url, {
-            headers: {
-                'Access-Control-Allow-Origin': 'http://localhost:3000'
-            }
-        });
-
-        dispatch({type: 'FETCH_PEOPLE', payload: response.data.results})
-    };
-
 export const fetchPeopleByPage = page =>
     async dispatch => {
         const proxyurl = "https://cors-anywhere.herokuapp.com/";
